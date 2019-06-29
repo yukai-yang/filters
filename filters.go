d2b393da1c6308ce128073d9f8911fc09d53364c
Yukai Yang <yukai.yang@gmail.com>
2019-06-29 23:15:12 +0200

    struct Kalman members

--- filters.go
+++ filters.go
@@ -1,6 +1,8 @@
 // Package filters implements several filtering algorithms
 package filters
 
+import "gonum.org/v1/gonum/mat"
+
 /* the interface */
 
 // Filter defines the common methods of the filters
@@ -9,4 +11,9 @@ type Filter interface {
 
 // Kalman defines the structure of the Kalman filter
 type Kalman struct {
+	// contains filtered or unexported fields
+	obs  *mat.Dense
+	parF *mat.Dense
+	parB *mat.Dense
+	parH *mat.Dense
 }
