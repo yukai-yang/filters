979efc94289cf4bdb2a2612b8103b747f12df700
Yukai Yang <yukai.yang@gmail.com>
2019-06-30 21:06:19 +0200

    utils.go in Kalman

---
+++ kalman/utils.go
@@ -0,0 +1,11 @@
+package kalman
+
+/* utility functions */
+
+// NewKalman initializes the Kalman filter
+// and returns the address of an object of Kalman struct
+func NewKalman() *Kalman {
+	var kal *Kalman
+
+	return kal
+}
